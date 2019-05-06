abe3ddd44c5a668a0539dd1cd9d3ee8ec7892498
Edoardo Carpita <edoardo.carpita@depsys.ch>
2019-05-06 17:19:30 +0200

    his history for him

---
+++ src/animalType/animalType.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package animalType;
+
+/**
+ *
+ * @author Edd993Surface
+ */
+public enum animalType {
+    
+    CRAPAUD("Crapaud"),
+    TRITON("Triton");
+
+    private final String animalName;
+
+    animalType(String animalName) {
+        this.animalName = animalName;
+    }
+    
+    public String toString(){
+        return animalName;
+    }
+    
+
+}
