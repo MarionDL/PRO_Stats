38738ed08dbade50c896fa3fd2bc5ccd6464aac4
Marion Dutu <marion.dutulaunay@heig-vd.ch>
2019-05-09 17:17:00 +0200

    Bar chart and Line chart added (yuhuuu)

--- src/charts/MainApp.java
+++ src/charts/MainApp.java
@@ -21,6 +21,12 @@ import javax.swing.*;
 
 import jsonstatisticsparser.*;
 import animalType.*;
+import java.util.Arrays;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.StackedBarChart;
+import javafx.scene.chart.XYChart;
 
 /**
  *
@@ -63,7 +69,7 @@ public class MainApp extends JFrame {
 
         /*
         * Creation de la Pie Chart principale
-        * */
+        */
         Group pieChartGroup = createMainPieChart();
         root.getChildren().add(pieChartGroup);
 
@@ -72,6 +78,18 @@ public class MainApp extends JFrame {
          */
         Group sideInfosGroup = createSideInfosZone();
         root.getChildren().add(sideInfosGroup);
+        
+        /*
+         * Creation de la Line Chart mois/animaux 
+         */
+        Group lineChartGroup = createLineChart();
+        root.getChildren().add(lineChartGroup);
+        
+        /*
+         * Creation de la Bar Chart mois/animaux 
+         */
+        Group barChartGroup = createBarChart();
+        root.getChildren().add(barChartGroup);
 
         return (scene);
     }
@@ -106,7 +124,7 @@ public class MainApp extends JFrame {
         chart.setLegendVisible(false);
         chart.setTitleSide(Side.BOTTOM);
 
-        // Change colors : faut le faire apres avoir affiché la scene
+        // Couleurs des tranches
         pieChartData.get(0).getNode().setStyle("-fx-pie-color: #8EFA9C;");
         pieChartData.get(1).getNode().setStyle("-fx-pie-color: #3F319B;");
         pieChartData.get(2).getNode().setStyle("-fx-pie-color: #180F50;");
@@ -118,6 +136,123 @@ public class MainApp extends JFrame {
         return pieChartGroup;
     }
     
+    /*
+     * Cree un groupe avec la line chart mois/animaux
+     */
+    public static Group createLineChart() {
+        Group lineChartGroup = new Group();
+        
+        // Definition des axes
+        final CategoryAxis xAxis = new CategoryAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        
+        final LineChart<String, Number> lineChart = new LineChart<>(xAxis, yAxis);
+                
+        lineChart.setTitle("Number of animals per month");
+        // Positionnement a gauche de la fenetre
+        lineChart.setLayoutX(0);
+        lineChart.setLayoutY(500);
+        lineChart.setLegendVisible(false);
+        
+        XYChart.Series series = new XYChart.Series();
+        
+        // Remplissage du graphe
+        series.getData().add(new XYChart.Data("Jan", 23));
+        series.getData().add(new XYChart.Data("Feb", 14));
+        series.getData().add(new XYChart.Data("Mar", 15));
+        series.getData().add(new XYChart.Data("Apr", 24));
+        series.getData().add(new XYChart.Data("May", 34));
+        series.getData().add(new XYChart.Data("Jun", 36));
+        series.getData().add(new XYChart.Data("Jul", 22));
+        series.getData().add(new XYChart.Data("Aug", 45));
+        series.getData().add(new XYChart.Data("Sep", 43));
+        series.getData().add(new XYChart.Data("Oct", 17));
+        series.getData().add(new XYChart.Data("Nov", 29));
+        series.getData().add(new XYChart.Data("Dec", 25));
+        
+        lineChart.getData().add(series);
+        lineChartGroup.getChildren().add(lineChart);
+        
+        return lineChartGroup;
+    }
+    
+    /*
+     * Cree un groupe avec la bar chart mois/animaux
+     */
+    public static Group createBarChart() {
+        Group barChartGroup = new Group();
+        
+        // Definition des axes
+        final CategoryAxis xAxis = new CategoryAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        
+        StackedBarChart<String, Number> sbc = new StackedBarChart<>(xAxis, yAxis);
+        sbc.setTitle("Number of animals per month");
+        xAxis.setCategories(FXCollections.<String>observableArrayList(
+                Arrays.asList("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
+                        "Aug", "Sep", "Oct", "Nov", "Dec")));
+        sbc.setLayoutX(500);
+        sbc.setLayoutY(500);
+        
+        // Definition des series (type d'animal)
+        XYChart.Series<String, Number> seriesTriton = new XYChart.Series<>();
+        XYChart.Series<String, Number> seriesFrog = new XYChart.Series<>();
+        XYChart.Series<String, Number> seriesToad = new XYChart.Series<>();
+        seriesTriton.setName("Tritons");
+        seriesFrog.setName("Frogs");
+        seriesToad.setName("Toads");
+        
+        // Donnees sur les tritons
+        seriesTriton.getData().add(new XYChart.Data<>("Jan", 25));
+        seriesTriton.getData().add(new XYChart.Data<>("Feb", 5));
+        seriesTriton.getData().add(new XYChart.Data<>("Mar", 23));
+        seriesTriton.getData().add(new XYChart.Data<>("Apr", 45));
+        seriesTriton.getData().add(new XYChart.Data<>("May", 45));
+        seriesTriton.getData().add(new XYChart.Data<>("Jun", 25));
+        seriesTriton.getData().add(new XYChart.Data<>("Jul", 5));
+        seriesTriton.getData().add(new XYChart.Data<>("Aug", 23));
+        seriesTriton.getData().add(new XYChart.Data<>("Sep", 45));
+        seriesTriton.getData().add(new XYChart.Data<>("Oct", 45));
+        seriesTriton.getData().add(new XYChart.Data<>("Nov", 25));
+        seriesTriton.getData().add(new XYChart.Data<>("Dec", 5));
+        
+        // Donnees sur les crapauds
+        seriesToad.getData().add(new XYChart.Data<>("Jan", 40));
+        seriesToad.getData().add(new XYChart.Data<>("Feb", 7));
+        seriesToad.getData().add(new XYChart.Data<>("Mar", 38));
+        seriesToad.getData().add(new XYChart.Data<>("Apr", 50));
+        seriesToad.getData().add(new XYChart.Data<>("May", 50));
+        seriesToad.getData().add(new XYChart.Data<>("Jun", 50));
+        seriesToad.getData().add(new XYChart.Data<>("Jul", 38));
+        seriesToad.getData().add(new XYChart.Data<>("Aug", 34));
+        seriesToad.getData().add(new XYChart.Data<>("Sep", 32));
+        seriesToad.getData().add(new XYChart.Data<>("Oct", 25));
+        seriesToad.getData().add(new XYChart.Data<>("Nov", 6));
+        seriesToad.getData().add(new XYChart.Data<>("Dec", 8));
+        
+        // Donnees sur les grenouilles
+        seriesFrog.getData().add(new XYChart.Data<>("Jan", 50));
+        seriesFrog.getData().add(new XYChart.Data<>("Feb", 12));
+        seriesFrog.getData().add(new XYChart.Data<>("Mar", 22));
+        seriesFrog.getData().add(new XYChart.Data<>("Apr", 34));
+        seriesFrog.getData().add(new XYChart.Data<>("May", 55));
+        seriesFrog.getData().add(new XYChart.Data<>("Jun", 66));
+        seriesFrog.getData().add(new XYChart.Data<>("Jul", 34));
+        seriesFrog.getData().add(new XYChart.Data<>("Aug", 36));
+        seriesFrog.getData().add(new XYChart.Data<>("Sep", 39));
+        seriesFrog.getData().add(new XYChart.Data<>("Oct", 25));
+        seriesFrog.getData().add(new XYChart.Data<>("Nov", 10));
+        seriesFrog.getData().add(new XYChart.Data<>("Dec", 8));
+        
+        sbc.getData().addAll(seriesTriton, seriesFrog, seriesToad);
+        barChartGroup.getChildren().add(sbc);
+        
+        return barChartGroup;
+    }
+    
+    /*
+     * Cree un groupe avec la zone d'infos droite
+     */
     public static Group createSideInfosZone() {
         
         Group sideInfosGroup = new Group();
